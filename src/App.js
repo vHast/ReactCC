d023b98445aa064dbe60f112606babcb35305f68
vHast <fedvhast@gmail.com>
2022-06-27 13:35:23 +0300

    React props and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 
+const Person = (props) => {
+  return (
+    <>
+      <h1>Name: {props.name}</h1>
+      <h2>Last Name: {props.lastName}</h2>
+      <h2>Age: {props.age}</h2>
+    </>
+  )
+}
+
 const App = () => {
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <Person name = {'John'} lastName = {"Doe"} age = "30" />
+      <Person name = {'Mary'} lastName = {"Doe"} age = "25" />
     </div>
   );
 }
